57d45fbac382fcee5a823e9e02870b6786120d4d
Bobby Shunk <Bobby@MacBook-Pro-5.local>
2017-07-14 10:48:50 -0500

    go back to the drawing board with JS

--- cards.js
+++ cards.js
@@ -1,28 +1,17 @@
 // Create an HTML page that contains a text area and a button labeled Create.
 // When the user enters in text into the text area and then clicks the create button, create a new card element in the DOM that includes it's own delete button. You decide the height/width of the card.
 // When the user clicks the Delete button, the containing card, and no other cards, should then be removed from the DOM. Not just made invisible, actually removed from the DOM.
-console.log("greetings.")
 
 // created variables to hold elements from html
 let input = document.getElementById("userInput");
 let createBtn = document.getElementById("createBtn");
 let outputCard = document.getElementById("cardsOutput");
-let del = document.getElementsByClassName("deleteBtn")
-let deleteCard = 
 
 // adds a click event listner to the create button and runs function createdCard when clicked
 createBtn.addEventListener("click", createdCard);
 
 // defines function createdCard
 function createdCard() {
-	// sets new variable output to equal 
-	let output = "<div class="card"><section><p class="text">" + input.value + "</p></section><input type="button" class="deleteBtn" value="Delete"></div";
-	outputCard.innerHTML += output;
-	for (var i = 0; i < del.length; i++) {
-		del[i].addEventListener("click", deleteCard)
-	}
-};
-
 
 
 
